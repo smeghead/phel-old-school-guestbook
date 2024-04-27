f71df87c2f0119533f9ea0e25c1f23330ab6b591
smeghead <smeghead7@gmail.com>
2024-04-27 19:06:35 +0900

    feat: post message and show messages.

--- build/initdb.d/001-init-db.sql
+++ build/initdb.d/001-init-db.sql
@@ -5,7 +5,7 @@ use guestbook;
 drop table if exists messages;
 
 create table messages (
-  id int(10) not null primary key,
+  id int(10) not null primary key auto_increment,
   name varchar(40) not null,
   message varchar(256)
 );
